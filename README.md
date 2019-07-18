be064bd5bd149bbdbbaf71471a07a782c9d4599e
Amar Parmar <amar.parmar@businessoffashion.com>
2019-07-18 10:15:36 +0100

    Some corrections to make this work

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # What is this?
 Playground to test different testing frameworks. The goal is for comparison of learnings. So its a fair test I have created a simple service which has another service injected, a public method, and a private method.
+
+# Install
+Install is just simple, just run:
+> composer install
+
+# Running Tests
+There are two test frameworks setup already:
+
+### PHP Unit
+To run the phpunit suite, use the following command:
+> bin/phpunit --verbose tests
+
+
+### PHP Sopec
+To run the php spec suite, use the following command:
+> bin/phpspec run
--- spec/BOF/ExampleQueryServiceSpec.php
+++ spec/BOF/ExampleQueryServiceSpec.php
@@ -27,27 +27,6 @@ class ExampleQueryServiceSpec extends ObjectBehavior
         $this->query([])->shouldReturn(['CASE_A', 'CASE_B', 'CASE_B']);
     }
 
-    function it_returns_expected_results_on_more_than_one_result()
-    {
-        $repo = $this->getMockEntityRepository([1, 2, 3]);
-        $this->beConstructedWith($repo);
-        $this->query([])->shouldReturn(['CASE_A', 'CASE_B', 'CASE_B']);
-    }
-
-    function it_returns_expected_results_on_more_than_one_result()
-    {
-        $repo = $this->getMockEntityRepository([1, 2, 3]);
-        $this->beConstructedWith($repo);
-        $this->query([])->shouldReturn(['CASE_A', 'CASE_B', 'CASE_B']);
-    }
-
-    function it_returns_expected_results_on_more_than_one_result()
-    {
-        $repo = $this->getMockEntityRepository([1, 2, 3]);
-        $this->beConstructedWith($repo);
-        $this->query([])->shouldReturn(['CASE_A', 'CASE_B', 'CASE_B']);
-    }
-
     private function getMockEntityRepository($return)
     {
         $prophet = new \Prophecy\Prophet();
